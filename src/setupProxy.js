4e69331ed07ffe1eb358845ff13e105996e3645a
Anton Hlinisty <anton.hlinisty@gmail.com>
2026-03-13 21:49:16 -0400

    Add google oauth

---
+++ src/setupProxy.js
@@ -0,0 +1,12 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  app.use(
+    "/api",
+    createProxyMiddleware({
+      target: process.env.REACT_APP_API_PROXY_TARGET || "http://localhost:8080",
+      changeOrigin: true,
+      secure: false,
+    })
+  );
+};
