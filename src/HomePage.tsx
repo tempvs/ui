e4c011906c8a8837ff77504d4020b95f9be9af1d
Anton Hlinisty <anton.hlinisty@gmail.com>
2026-05-09 17:26:20 -0400

    Update home page

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,15 +1,34 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
+import { FaBook, FaSignInAlt } from 'react-icons/fa';
+import Cookies from 'js-cookie';
+import { Navigate } from 'react-router-dom';
+
+type IconProps = {
+  className?: string;
+};
+
+const BookIcon = FaBook as React.ComponentType<IconProps>;
+const SignInIcon = FaSignInAlt as React.ComponentType<IconProps>;
 
 export default function HomePage() {
+  const loggedIn = Cookies.get('TEMPVS_LOGGED_IN');
+
+  if (loggedIn) {
+    return <Navigate to="/profile" replace />;
+  }
+
   return (
-    <Container fluid>
-      <Row>
-        <Col sm={4} />
-        <Col sm={4}>
-          Hello
+    <Container fluid className="home-shell px-4 px-xl-5">
+      <Row className="justify-content-center">
+        <Col xl={8} lg={9} md={10}>
+          <div className="home-message-panel">
+            <p className="home-message">
+              To see the library, press <span className="home-inline-icon"><BookIcon /></span>. To login/register, press the{' '}
+              <span className="home-inline-icon"><SignInIcon /></span>.
+            </p>
+          </div>
         </Col>
-        <Col sm={4} />
       </Row>
     </Container>
   );
